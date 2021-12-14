fe791e682b4d5ce3ab9d1e45dfd25de310e0f83f
andres9630 <andres159630@hotmail.com>
2021-12-13 21:53:41 -0600

    Modificando proyecto

---
+++ Proyecto/src/proyecto/DatosException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luis;
+
+/**
+ *
+ * @author Kuis Rodolfo
+ */
+public class DatosException extends Exception{
+
+    public DatosException(String message) {
+        super(message);
+    }
+    
+}
